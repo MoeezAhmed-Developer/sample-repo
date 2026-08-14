ee3ad14d94fe64d166fc4fdb5a03a498db15a9ae
Muhammad Moeez <moeezahmed1012@gmail.com>
2026-08-14 23:50:54 +0500

    Initial Commit

---
+++ api.js
@@ -0,0 +1,29 @@
+// USer API
+// const URL = "https://jsonplaceholder.typicode.com/users";
+
+// Quote APi
+// https://dummyjson.com/quotes
+
+// Dogs
+// https://dog.ceo/api/breeds/list/all
+
+// fetch(URL)
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     // console.log(data);
+//     data.forEach((user) => {
+//       console.log(user.id);
+//       console.log(user.name);
+//       console.log(user.email);
+//     });
+//   });
+
+// const fetchUSer = async () => {
+//   const RESPONSE = await fetch(URL);
+//   const DATA = await RESPONSE.json();
+//   console.log(DATA[2].address.geo.lat);
+// };
+
+// fetchUSer();
